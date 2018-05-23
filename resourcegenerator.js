683b2fc27a57a749e7c7dd45a1248dffba55c874
GoHod <20615668+GoHod@users.noreply.github.com>
2018-05-23 13:55:53 +0100

    initial commit

---
+++ resourcegenerator.js
@@ -0,0 +1,26 @@
+/** csv file
+a,b,c
+1,2,3
+4,5,6
+*/
+const csvFilePath='DRLContentSheetCSV.csv'
+const csv=require('csvtojson')
+let results=[]
+csv()
+.fromFile(csvFilePath)
+.on('json',(jsonObj)=>{
+  const result=Object.assign({}, jsonObj);
+  result.category = jsonObj.category.split('\n');
+  result.groups = jsonObj.groups.split('\n');
+  let sector = {}
+  sector[jsonObj.sectorID]=jsonObj.sectorDescription
+  result.sector = sector
+  delete result.sectorID
+  delete result.sectorDescription
+    // combine csv header row and csv line to a json object
+    // jsonObj.a ==> 1 or 4
+    results.push(result)
+})
+.on('done',(error)=>{
+    console.log(results)
+})
