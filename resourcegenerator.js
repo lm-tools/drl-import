33538260256f8aef92fd768bb17b4dda81502a38
mangogrin <37105143+mangogrin@users.noreply.github.com>
2018-07-04 13:29:20 +0100

    @OR-1222: to handle empty sector values

--- resourcegenerator.js
+++ resourcegenerator.js
@@ -1,26 +1,28 @@
 /** csv file
-a,b,c
-1,2,3
-4,5,6
-*/
-const csvFilePath='DRLContentSheetCSV.csv'
-const csv=require('csvtojson')
-let results=[]
+ a,b,c
+ 1,2,3
+ 4,5,6
+ */
+const csvFilePath = 'DRLContentSheetCSV.csv';
+const csv = require('csvtojson');
+const results = [];
 csv()
-.fromFile(csvFilePath)
-.on('json',(jsonObj)=>{
-  const result=Object.assign({}, jsonObj);
-  result.category = jsonObj.category.split('\n');
-  result.groups = jsonObj.groups.split('\n');
-  let sector = {}
-  sector[jsonObj.sectorID]=jsonObj.sectorDescription
-  result.sector = sector
-  delete result.sectorID
-  delete result.sectorDescription
+  .fromFile(csvFilePath)
+  .on('json', (jsonObj) => {
+    const result = Object.assign({}, jsonObj);
+    result.category = jsonObj.category.split('\n');
+    result.groups = jsonObj.groups.split('\n');
+    const sector = {};
+    if (jsonObj.sectorID) {
+      sector[jsonObj.sectorID] = jsonObj.sectorDescription;
+    }
+    result.sector = sector;
+    delete result.sectorID;
+    delete result.sectorDescription;
     // combine csv header row and csv line to a json object
     // jsonObj.a ==> 1 or 4
-    results.push(result)
-})
-.on('done',(error)=>{
-    console.log(results)
-})
+    results.push(result);
+  })
+  .on('done', () => {
+    console.log(results);
+  });
